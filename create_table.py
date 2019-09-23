34c3c49249c1306dcbc3088895d94235c11d9f82
Brian Livingston <ak.brian09@gmail.com>
2019-09-23 15:32:33 -0500

    added create_table

---
+++ create_table.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+basedir = os.getcwd()
+outdir = os.path.join(basedir,'output')
+
+pkls = []
+files = os.listdir(outdir)
+for file in files:
+	if file.endswith('.pkl'):
+		pkls.append(file)
+
+pkls = sorted(pkls)
+pkls = [os.path.join(outdir,pkl) for pkl in pkls]
+
+series_list = []
+for pkl in pkls:
+	series_list.append(pd.read_pickle(pkl))
+
+df = pd.concat(series_list, axis=1)
+
+xlpath = os.path.join(outdir,'table.xlsx')
+df.to_excel(xlpath, freeze_panes=(0,0), header=False)
