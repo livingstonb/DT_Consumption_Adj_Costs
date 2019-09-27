8094caae84878bbe7c577c158d5c657b647a232d
Brian Livingston <livingstonb@uchicago.edu>
2019-09-27 18:08:56 -0500

    changes to sbatch scripts

--- server1.sh
+++ server1.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=/home/livingstonb/GitHub/DT_Consumption_Adj_Costs/output/run%a.err
 #SBATCH --partition=broadwl
 #SBATCH --array=0-59
-#SBATCH --time=6:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=6000
+#SBATCH --mem-per-cpu=4000
 
 python -u master.py $SLURM_ARRAY_TASK_ID
--- server2.sh
+++ server2.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=/home/livingstonb/GitHub/DT_Consumption_Adj_Costs/output/run%a.err
 #SBATCH --partition=broadwl
 #SBATCH --array=60-84
-#SBATCH --time=6:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=8000
+#SBATCH --mem-per-cpu=6000
 
 python -u master.py $SLURM_ARRAY_TASK_ID
